2dad9531d76b9ef3987ba0e8b6292d84fe75568d
Eka Cahya Putra <ekacahyaps@gmail.com>
2023-01-24 15:01:19 +0700

    chore: create product model

--- features/product/data/model.go
+++ features/product/data/model.go
@@ -1,1 +1,41 @@
 package data
+
+import (
+	"e-commerce/features/product"
+
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Name        string
+	Price       uint
+	Quantity    uint
+	Description string
+	Image       string
+	UserID      uint
+}
+
+func DataToCore(data Product) product.Core {
+	return product.Core{
+		ID:          data.ID,
+		Name:        data.Name,
+		Price:       data.Price,
+		Quantity:    data.Quantity,
+		Description: data.Description,
+		Image:       data.Image,
+		UserID:      data.UserID,
+	}
+}
+
+func CoreToData(data product.Core) Product {
+	return Product{
+		Model:       gorm.Model{ID: data.ID},
+		Name:        data.Name,
+		Price:       data.Price,
+		Quantity:    data.Quantity,
+		Description: data.Description,
+		Image:       data.Image,
+		UserID:      data.UserID,
+	}
+}
