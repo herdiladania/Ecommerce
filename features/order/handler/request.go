c7fe2439f1cc515c504f6f8ca3c0bffd786f43c2
Herdiladania <herdiladania11@gmail.com>
2023-01-27 02:15:52 +0700

    feat : add order request (cartid and adress

--- features/order/handler/request.go
+++ features/order/handler/request.go
@@ -1,1 +1,21 @@
 package handler
+
+import "e-commerce/features/order"
+
+type OrderRequest struct {
+	CartID  uint   `json:"cart_id" form:"cart_id"`
+	Address string `json:"address" form:"address"`
+}
+
+func ToCore(data interface{}) *order.Core {
+	res := order.Core{}
+
+	switch data.(type) {
+	case OrderRequest:
+		cnv := data.(OrderRequest)
+		res.CartID = cnv.CartID
+		res.Address = cnv.Address
+	}
+
+	return &res
+}
