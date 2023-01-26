89521dc9ebbdfb9863635e237c3ca88e09b9698c
Herdiladania <herdiladania11@gmail.com>
2023-01-26 17:02:30 +0700

    feat : make add transaction query logic

---
+++ features/order/data/query.go
@@ -0,0 +1,89 @@
+package data
+
+import (
+	"e-commerce/config"
+	"e-commerce/features/order"
+
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/midtrans/midtrans-go"
+	"github.com/midtrans/midtrans-go/snap"
+	"gorm.io/gorm"
+)
+
+type orderQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) order.OrderData {
+	return &orderQuery{
+		db: db,
+	}
+}
+func (oq *orderQuery) Add(userID uint, totalPrice float64) (order.Core, string, error) {
+	transaksi := oq.db.Begin()
+
+	cart := []userCart{}
+	if err := transaksi.Where("user_id = ?", userID).Find(&cart).Error; err != nil {
+		transaksi.Rollback()
+		log.Println("error retrieve user cart: ", err.Error())
+		return order.Core{}, "", err
+	}
+
+	orderInput := Order{
+		UserID:          userID,
+		Status:          "Waiting For Payment",
+		TransactionDate: time.Now(),
+		TotalPrice:      totalPrice,
+	}
+
+	product := userProduct{}
+	transaksi.First(&product, cart[0].ProductID)
+	orderInput.SellerID = product.UserID
+
+	if err := transaksi.Create(&orderInput).Error; err != nil {
+		transaksi.Rollback()
+		log.Println("error add order query: ", err.Error())
+		return order.Core{}, "", err
+	}
+
+	orderInput.TransactionCode = "Transaction-" + fmt.Sprint(orderInput.ID)
+	transaksi.Save(&orderInput)
+
+	listProducts := []orderProduct{}
+	for _, item := range cart {
+		orderProduct := orderProduct{
+			OrderID:    orderInput.ID,
+			ProductID:  item.ProductID,
+			Quantity:   item.Quantity,
+			TotalPrice: item.TotalPrice,
+		}
+		listProducts = append(listProducts, orderProduct)
+	}
+	if err := transaksi.Create(&listProducts).Error; err != nil {
+		transaksi.Rollback()
+		log.Println("error create orderproduct: ", err.Error())
+		return order.Core{}, "", err
+	}
+
+	if err := transaksi.Where("user_id = ?", userID).Delete(userCart{}).Error; err != nil {
+		transaksi.Rollback()
+		log.Println("error delete user cart: ", err.Error())
+		return order.Core{}, "", err
+	}
+
+	s := config.MidtransSnapClient()
+	req := &snap.Request{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  orderInput.TransactionCode,
+			GrossAmt: int64(totalPrice),
+		},
+	}
+	snapResp, _ := s.CreateTransaction(req)
+
+	transaksi.Commit()
+
+	return DataToCore(orderInput), snapResp.RedirectURL, nil
+}
