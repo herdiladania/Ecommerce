46b18d2dc36c34156367b849b3905b9fdb08cdd8
Herdiladania <herdiladania11@gmail.com>
2023-01-26 16:59:58 +0700

    feat : add sellerid to core

--- features/order/entity.go
+++ features/order/entity.go
@@ -1,40 +1,45 @@
 package order
 
-import "github.com/labstack/echo"
+import (
+	"time"
+
+	"github.com/labstack/echo"
+)
 
 type Core struct {
 	ID              uint
 	UserID          uint
+	SellerID        uint
 	CartID          uint
 	Address         string
 	Quantity        int
-	TotalPrice      int
+	TotalPrice      float64
 	PaymentUrl      string
 	TransactionCode string
-	TransactionDate string
+	TransactionDate time.Time
 	Status          string
 }
 
 type OrderHandler interface {
 	Add() echo.HandlerFunc
-	OrderHistory() echo.HandlerFunc
-	SellingHistory() echo.HandlerFunc
-	UpdateOrderStatus() echo.HandlerFunc
-	DeleteOrder() echo.HandlerFunc
+	// OrderHistory() echo.HandlerFunc
+	// SellingHistory() echo.HandlerFunc
+	// UpdateOrderStatus() echo.HandlerFunc
+	// DeleteOrder() echo.HandlerFunc
 }
 
 type OrderService interface {
 	Add(token interface{}, totalPrice float64) (Core, string, error)
-	OrderHistory(token interface{}) ([]Core, error)
-	SellingHistory(token interface{}) ([]Core, error)
-	UpdateOrderStatus(token interface{}, orderID uint, updatedStatus int) error
-	DeleteOrder(token interface{}, orderID uint) error
+	// OrderHistory(token interface{}) ([]Core, error)
+	// SellingHistory(token interface{}) ([]Core, error)
+	// UpdateOrderStatus(token interface{}, orderID uint, updatedStatus int) error
+	// DeleteOrder(token interface{}, orderID uint) error
 }
 
 type OrderData interface {
 	Add(userId uint, totalPrice float64) (Core, string, error)
-	OrderHistory(userId uint) ([]Core, error)
-	SellingHistory(userId uint) ([]Core, error)
-	UpdateOrderStatus(userID uint, orderID uint, updatedStatus int) error
-	DeleteOrder(userID uint, orderID uint) error
+	// OrderHistory(userId uint) ([]Core, error)
+	// SellingHistory(userId uint) ([]Core, error)
+	// UpdateOrderStatus(userID uint, orderID uint, updatedStatus int) error
+	// DeleteOrder(userID uint, orderID uint) error
 }
